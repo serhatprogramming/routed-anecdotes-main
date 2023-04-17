568157046492f7a2931c14a42c7b3ea489edd2db
serhatprogramming <serhatprogramming@gmail.com>
2023-04-17 18:18:50 -0400

    refactor with useField custom hook

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
   useNotificationValue,
 } from "./NotificationContext";
 
+import { useField } from "./hooks";
+
 const Menu = () => {
   const padding = {
     paddingRight: 5,
@@ -87,21 +89,24 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const navigate = useNavigate();
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const notificationDispatch = useNotificationDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
-    notificationDispatch({ type: "ADD", payload: content });
+    notificationDispatch({
+      type: "ADD",
+      payload: `${content.value} is added.`,
+    });
     setTimeout(() => {
       notificationDispatch({ type: "CLEAR" });
     }, 5000);
@@ -114,27 +119,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
